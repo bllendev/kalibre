323b69ca08cc045dffa83c358dc2c98ee55cdf16
bllen <37097946+AllenGarza@users.noreply.github.com>
2022-06-16 08:56:56 -0500

    new buttons

--- books/utils.py
+++ books/utils.py
@@ -63,24 +63,12 @@ class LibgenAPI:
             return book_list
 
     def _get_title_choices(self):
-        title_filters = {"Extension": "epub"}
-        titles = self.libgen.search_title_filtered(self.title_search, title_filters)
-        authors = self.libgen.search_author_filtered(self.title_search, title_filters)
-        # titles = self.libgen.search_title(self.title_search)
-        # authors = self.libgen.search_authors(self.title_search)
+        title_filters = {"Extension": "mobi"}
+        # titles = self.libgen.search_title_filtered(self.title_search, title_filters, exact_match=False)
+        # authors = self.libgen.search_author_filtered(self.title_search, title_filters, exact_match=False)
+        titles = self.libgen.search_title(self.title_search)
+        authors = self.libgen.search_author(self.title_search)
         return titles + authors
-
-    def get_title_download_link(self, title):
-        download_links = self.libgen.resolve_download_links(title)
-        return download_links
-
-    def get_first_download_link(self):
-        try:
-            first = self._get_title_choices()[0]
-        except Exception as e:
-            print(f"ERROR: {e}")
-            print(f"self._get_title_choices(): {self._get_title_choices()}")
-        return self.get_title_download_link(first)
 
     def download_book(self):
         pass
