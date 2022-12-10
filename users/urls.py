b2040cbe605b9a7a59b4d9cfc7388338c4501227
bllen <37097946+AllenGarza@users.noreply.github.com>
2022-12-10 15:11:44 -0700

    gifs !!!

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,7 @@
 # from django.urls import path
-# from users.views import SignupPageView
+# from users import ajax
 
 
 # urlpatterns = [
-#     path('signup/', SignupPageView.as_view(), name='signup'),
-#     # emails
-#     path("my_emails/", views.my_emails, name="my_emails"),
-#     path("add_email/", views.add_email, name="add_email"),
+#     path("add_email/", ajax.add_email, name="add_email"),
 # ]
