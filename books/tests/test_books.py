a62e8792543dd6b6794f0734fba71fcce1e51600
Allen Garza <113651082+bllendev@users.noreply.github.com>
2023-03-11 22:55:12 -0700

    test comment

--- books/tests/test_books.py
+++ books/tests/test_books.py
@@ -1,23 +1,23 @@
-from django.test import TestCase
-from django.urls import reverse
-from django.contrib.auth.models import User
-import factory
+# from django.test import TestCase
+# from django.urls import reverse
+# from django.contrib.auth.models import User
+# import factory
 
-from django.conf import settings
+# from django.conf import settings
 
 
 
-class TestBook(TestCase):
+# class TestBook(TestCase):
 
-    def setUp(self):
-        # book set up
-        pass
+#     def setUp(self):
+#         # book set up
+#         pass
 
-    def test_book_init(self):
-        pass
+#     def test_book_init(self):
+#         pass
 
-    def test_book_send(self):
-        pass
+#     def test_book_send(self):
+#         pass
 
-    def test_book_methods(self):
-        pass
+#     def test_book_methods(self):
+#         pass
