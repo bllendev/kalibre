b1e5c9966197d1c5ff813671467746b02682adb6
bllen <bllendev@gmail.com>
2023-04-27 18:57:15 -0600

    working translate prototype, removing bad print statements

--- books/_translate.py
+++ books/_translate.py
@@ -2,7 +2,7 @@ import os
 import json
 import requests
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 import ebooklib
 from ebooklib import epub
 from googletrans import Translator
@@ -19,10 +19,13 @@ class EbookTranslate:
     ... fastapi - homemade transformer model on fastapi infrastructure
     ... else - local huggingface transformer model
     """
-    def __init__(self, epub_path, language, google_api=True, openai=False, fastapi=False):
+    def __init__(self, epub_path, language, google_api=True, openai=False, fastapi=False, test=True):
         # key params
         self.epub_path = epub_path
         self.book = epub.read_epub(epub_path)
+        self.test = test
+        self.translation_cache = {}
+        self.cached_used = 0
 
         # load models / apis
         self.fastapi = False
@@ -45,80 +48,116 @@ class EbookTranslate:
             self.tokenizer = None
 
     def filter_text(self, text):
-        # Regular expression to match alphanumeric characters and spaces
-        pattern = re.compile(r'[^\w\s]')
-        # Replace everything else with an empty string
-        filtered_text = pattern.sub('', text)
-        # Split the filtered text into words
-        words = filtered_text.split()
-        # Join the words back into a string with spaces
-        filtered_text = ' '.join(words)
-        return filtered_text
+        return text.strip()
+        # # Regular expression to match alphanumeric characters and spaces
+        # pattern = re.compile(r'[^\w\s]')
+        # # Replace everything else with an empty string
+        # filtered_text = pattern.sub('', text)
+        # # Split the filtered text into words
+        # words = filtered_text.split()
+        # # Join the words back into a string with spaces
+        # filtered_text = ' '.join(words)
+        # return filtered_text
 
     def _extract_text(self, section):
-        if isinstance(section, epub.Section):
-            for item in section.items:
-                yield from self._extract_text(item)
-        elif isinstance(section, epub.EpubHtml):
+        if isinstance(section, epub.EpubHtml):
             soup = BeautifulSoup(section.content.decode('utf-8'), 'html.parser')
             for script in soup(["script", "style"]):
                 script.extract()
-            text = soup.get_text()
-            text = text.encode('ascii', 'ignore').decode()
-            text = re.sub(r'\n\s*\n', '\n', text)
-            yield self.filter_text(text)
+
+            texts = []
+            for text_node in soup.stripped_strings:
+                filtered_text = self.filter_text(text_node)
+                text_node_bs = NavigableString(filtered_text)
+                texts.append((text_node, text_node_bs))  # Save the text_node and its NavigableString counterpart
+
+            return texts
+        return []  # Return an empty list for non-EpubHtml sections
 
     def _translate_text(self, text):
+        if "xml version" in text:
+            return ""  # skip XML declarations (don't expose in text)
+
+        if text in self.translation_cache:
+            self.cached_used += 1
+            return self.translation_cache[text]
+
         # google translate api
         if self.google_api:
             translator = Translator()
-            return translator.translate(text, dest='es').text
+            translated_text = translator.translate(text, dest='es').text
         # openai not wired up yet... @AG++
-        if self.translation_api:
+        elif self.translation_api:
             r = requests.post(self.translation_api, data=json.dumps({'text': text}), headers={'Content-Type': 'application/json'})
             if r.status_code == 200:
-                return r.json()['translation']
+                translate_text = r.json()['translation']
             else:
                 raise ValueError(f"Translation API returned error code {r.status_code}")
         # local huggingface transformer model
         else:
-            print(f"text: {text[:100]}")
             encoded_text = self.tokenizer.encode(text, return_tensors="tf")
             generated = self.model.generate(encoded_text)
-            generated_text = self.tokenizer.decode(generated[0], skip_special_tokens=True)
-            return generated_text
+            translated_text = self.tokenizer.decode(generated[0], skip_special_tokens=True)
 
-    def _reinject_text(self, section, translated_text):
-        if isinstance(section, epub.Section):
-            for item in section.items:
-                self._reinject_text(item, translated_text)
-        elif isinstance(section, epub.EpubHtml):
-            soup = BeautifulSoup(section.content, 'html.parser')
-            new_content = ""
-            for text in translated_text.split('\n'):
-                new_content += f"<p>{text}</p>"
-            soup.body.clear()
-            soup.body.append(BeautifulSoup(new_content, 'html.parser'))
-            section.content = str(soup)
+        # Cache the translation
+        self.translation_cache[text] = translated_text
+        return translated_text
+
+    def _reinject_text(self, section):
+        if isinstance(section, epub.EpubHtml):
+            soup = BeautifulSoup(section.content.decode('utf-8'), 'html.parser')
+
+            # Helper function to traverse the tree, collect NavigableStrings and their parents
+            def collect_text_nodes(node, text_nodes):
+                if not hasattr(node, 'contents') or not node.contents:
+                    return
+                for child in node.contents:
+                    if isinstance(child, NavigableString):
+                        original_text = str(child).strip()
+                        if original_text:
+                            text_nodes.append((child.parent, child))
+                    else:
+                        collect_text_nodes(child, text_nodes)
+
+            text_nodes = []
+            collect_text_nodes(soup, text_nodes)
+
+            # Translate and replace text
+            for parent, original_text_node in text_nodes:
+                original_text = str(original_text_node).strip()
+                translated_text = self._translate_text(original_text)
+                new_text_node = NavigableString(translated_text)
+                original_text_node.replace_with(new_text_node)
+
+                if self.test:
+                    print(f"Original: {original_text}")
+                    print(f"Translated: {translated_text}")
+
+            # Update the section content with the modified soup
+            section.content = str(soup).encode('utf-8')
 
     def translate_ebook(self):
+        if self.test:
+            print("---------- TEST CASE --------")
+            print(f"Translating 1/5 of {self.book}...")
+            i = 0
+            i_test_threshold = len([item for item in self.book.get_items()]) / 6
+            print(f"i_test_threshold: {i_test_threshold}")
+
         for section in self.book.get_items():
-            text_chunks = [text_chunk.strip() for text_chunk in re.findall(r'\S.{0,510}\S(?=\s|$)|\S+', ''.join(self._extract_text(section)))]
-            translated_text = []
-            for chunk in text_chunks:
-                if len(chunk) <= 512:
-                    print(f"chunk: {chunk[:100]}")
-                    translated_text.append(self._translate_text(chunk))
-                else:
-                    subchunks = [subchunk.strip() for subchunk in re.findall(r'\S.{0,508}\S(?=\s|$)|\S+', chunk)]
-                    subchunk_translations = [self._translate_text(subchunk) for subchunk in subchunks]
-                    translated_text.append(' '.join(subchunk_translations))
-            print(translated_text)  # Add this line to see the contents of translated_text
-            self._reinject_text(section, ' '.join(translated_text))
+            if isinstance(section, epub.EpubHtml):
+                # test threshold - allows for quicker testing
+                if self.test:
+                    i += 1
+                    if i > i_test_threshold:
+                        break
+
+                self._reinject_text(section)
         return self.book
 
     def get_translated_book_path(self):
         translated_ebook = self.translate_ebook()
         translated_epub_path = self.epub_path.replace(".epub", f"_translated.epub")
         epub.write_epub(translated_epub_path, translated_ebook)
+        print(f"self.cached_used: {self.cached_used}")
         return translated_epub_path
