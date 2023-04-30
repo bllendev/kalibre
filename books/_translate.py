8a8f48ea7c0e2a41df02f0b14b7c1157e275442c
bllen <bllendev@gmail.com>
2023-04-30 16:28:01 -0600

    docs

--- books/_translate.py
+++ books/_translate.py
@@ -11,27 +11,32 @@ from bs4 import BeautifulSoup, NavigableString
 import ebooklib
 from ebooklib import epub
 from googletrans import Translator
-# from transformers import pipeline, AutoTokenizer, TFAutoModelForSeq2SeqLM  # not in Pipfile
 
 
 class EbookTranslate:
     """
-    4 primary methods of translation intended for the following class...
+    EbookTranslate class for translating ebooks.
+
+    - see Book._create_book_file() for usage.
+
+    3 primary methods of translation intended for the following class...
     ... google_api - Google translate API
     ... openai - OpenAI API
     ... fastapi - homemade transformer model on fastapi infrastructure
-    ... else - local huggingface transformer model
+
+    **test_mode*
+    - param:test_mode - Boolean - if True, shortens tests to
+    translating a 1/4 of the book, logs time, and cache results.
     """
-    def __init__(self, epub_path, language, google_api=True, openai=False, fastapi=False, test=True):
+    def __init__(self, epub_path, language, google_api=True, openai=False, fastapi=False, test_mode=True):
         # key params
         self.epub_path = epub_path
         self.book = epub.read_epub(epub_path)
-        self.test = test  # NOTE: TEST IS SET TO TRUE FOR NOW... @AG++
+        self.test_mode = test_mode  # NOTE: TEST IS SET TO TRUE FOR NOW... @AG++
 
         # caching
         redis_url = os.environ.get("REDISCLOUD_URL", "redis://localhost:6379")
         self.redis = redis.StrictRedis(host='redis', port=6379, db=0)
-
         self.cached_used = 0
 
         # load models / apis
@@ -49,49 +54,42 @@ class EbookTranslate:
             self.openai = True
             self.tokenizer = None
             self.translation_api = f"https://api.openai.com/v1/engines/davinci/completions"
-        # using my version of google translate api... @AG++
         elif google_api:
             self.google_api = True
             self.tokenizer = None
 
     def filter_text(self, text):
-        return text.strip()
-        # # Regular expression to match alphanumeric characters and spaces
-        # pattern = re.compile(r'[^\w\s]')
-        # # Replace everything else with an empty string
-        # filtered_text = pattern.sub('', text)
-        # # Split the filtered text into words
-        # words = filtered_text.split()
-        # # Join the words back into a string with spaces
-        # filtered_text = ' '.join(words)
-        # return filtered_text
+        """
+        filter out text that is not needed for translation
+        ... cuts down text by sentences (using punctuation) and strips
 
-    def _extract_text(self, section):
-        if isinstance(section, epub.EpubHtml):
-            soup = BeautifulSoup(section.content.decode('utf-8'), 'html.parser')
-            for script in soup(["script", "style"]):
-                script.extract()
-
-            texts = []
-            for text_node in soup.stripped_strings:
-                filtered_text = self.filter_text(text_node)
-                text_node_bs = NavigableString(filtered_text)
-                texts.append((text_node, text_node_bs))  # Save the text_node and its NavigableString counterpart
-
-            return texts
-        return []  # Return an empty list for non-EpubHtml sections
+        param text: str - text to be filtered
+        """
+        # Split the text by punctuation
+        text_chunks = re.split(r'([.!?])', text)
+        # Remove empty strings and leading/trailing whitespace
+        text_chunks = [chunk.strip() for chunk in text_chunks if chunk.strip()]
+        return text_chunks
 
     def _normalize_text(self, text):
-        # Normalize the text by lowercasing and removing punctuation.
-        # You can customize the normalization process as needed.
+        """
+        Normalize the text by lowercasing and removing punctuation.
+        You can customize the normalization process as needed.
+
+        param text: str - text to be normalized
+        """
         normalized_text = text.lower()
         normalized_text = re.sub(r'[^\w\s]', '', normalized_text)
         return normalized_text
 
     def _apply_formatting(self, original_text, translated_text):
-        # Apply the original formatting (punctuation, case-size) to the translated text
-        # This is a simple example, you may need to create a more sophisticated function
-        formatted_text = ""
+        """
+        Apply the original formatting (punctuation, case-size) to the translated text
+        NOTE: this only works with roman langauges, be warys
+
+        param original_text: str - og text to be used as formatting reference for translation
+        """
+        formatted_text = ""  # if self.language is roman... TODO: finish this (see docs)
         for orig_char, trans_char in zip(original_text, translated_text):
             if orig_char.isupper():
                 formatted_text += trans_char.upper()
@@ -100,42 +98,64 @@ class EbookTranslate:
 
         return formatted_text
 
-    def _translate_text(self, text):
-        if "xml version" in text:
-            return ""  # skip XML declarations (don't expose in text)
+    def _translate_text(self, text_chunks):
+        """
+        the translate method which translates a list of strings
+        ... delegates translation feature to 3 of the following methods
+        ... 1. google translate api
+        ... 2. fasapi transformer model (not finished yet)
+        ... 3. openai transformer model (not finished yet)
+        ... 4. local huggingface transformer model (disabled)
 
-        # Check the Redis cache for a translation
-        normalized_text = self._normalize_text(text)
-        translated_text = self.redis.get(normalized_text)
-        if translated_text:
-            self.cached_used += 1
-            translated_text = self._apply_formatting(text, translated_text.decode('utf-8'))
-            return translated_text
+        param text_chunks: list - list of strings to be translated
+        """
+        translated_chunks = []
 
-        # google translate api
-        if self.google_api:
-            translator = Translator()
-            translated_text = translator.translate(text, dest='es').text
+        for text in text_chunks:
+            if "xml version" in text:
+                continue  # skip XML declarations (don't expose in text)
 
-        # openai not wired up yet... @AG++
-        elif self.translation_api:
-            r = requests.post(self.translation_api, data=json.dumps({'text': text}), headers={'Content-Type': 'application/json'})
-            if r.status_code == 200:
-                translate_text = r.json()['translation']
+            # Check the Redis cache for a translation
+            normalized_text = self._normalize_text(text)
+            translated_text = self.redis.get(normalized_text)
+            if translated_text:
+                self.cached_used += 1
+                translated_text = self._apply_formatting(text, translated_text.decode('utf-8'))
+                return translated_text
+
+            # google translate api
+            if self.google_api:
+                translator = Translator()
+                translated_text = translator.translate(text, dest='es').text
+
+            # openai not wired up yet... @AG++
+            elif self.translation_api:
+                r = requests.post(self.translation_api, data=json.dumps({'text': text}), headers={'Content-Type': 'application/json'})
+                if r.status_code == 200:
+                    translate_text = r.json()['translation']
+                else:
+                    raise ValueError(f"Translation API returned error code {r.status_code}")
+
+            # local huggingface transformer model
             else:
-                raise ValueError(f"Translation API returned error code {r.status_code}")
+                encoded_text = self.tokenizer.encode(text, return_tensors="tf")
+                generated = self.model.generate(encoded_text)
+                translated_text = self.tokenizer.decode(generated[0], skip_special_tokens=True)
 
-        # local huggingface transformer model
-        else:
-            encoded_text = self.tokenizer.encode(text, return_tensors="tf")
-            generated = self.model.generate(encoded_text)
-            translated_text = self.tokenizer.decode(generated[0], skip_special_tokens=True)
+            # Cache the translation in Redis
+            self.redis.set(text, translated_text)
+            translated_chunks.append(translated_text)
 
-        # Cache the translation in Redis
-        self.redis.set(text, translated_text)
+        # Join the translated chunks
+        translated_text = ' '.join(translated_chunks)
         return translated_text
 
     def _reinject_text(self, section):
+        """
+        the reinject method which replaces the original text with the translated text
+
+        param section: epub.EpubHtml - the section to be translated
+        """
         if isinstance(section, epub.EpubHtml):
             soup = BeautifulSoup(section.content.decode('utf-8'), 'html.parser')
 
@@ -151,17 +171,23 @@ class EbookTranslate:
                     else:
                         collect_text_nodes(child, text_nodes)
 
+            # recursively collect all text nodes
             text_nodes = []
             collect_text_nodes(soup, text_nodes)
 
             # translate and replace text
             for parent, original_text_node in text_nodes:
+                # filter and translate text
                 original_text = str(original_text_node).strip()
-                translated_text = self._translate_text(original_text)
+                filtered_text_chunks = self.filter_text(original_text)
+                translated_text = self._translate_text(filtered_text_chunks)
+
+                # replace text node with translation
                 new_text_node = NavigableString(translated_text)
                 original_text_node.replace_with(new_text_node)
 
-                if self.test:
+                # test logs
+                if self.test_mode:
                     print(f"Original: {original_text}")
                     print(f"Translated: {translated_text}")
 
@@ -172,8 +198,11 @@ class EbookTranslate:
         """
         the primary function to 'translate' the ebook,
         translates text in place.
+
+        params: **test_mode** (ctrl-f)
         """
-        if self.test:
+        # test logs
+        if self.test_mode:
             print("------------------- TEST CASE ------------------")
             print(f"Translating 1/4 of {self.book}...")
             i = 0
@@ -183,7 +212,7 @@ class EbookTranslate:
         for section in self.book.get_items():
             if isinstance(section, epub.EpubHtml):
                 # test threshold - allows for quicker testing
-                if self.test:
+                if self.test_mode:
                     i += 1
                     if i > i_test_threshold:
                         break
@@ -202,19 +231,22 @@ class EbookTranslate:
         ... test::False then the entire book will be translated, and no logs
         """
         # set test
-        test = self.test if test is None else test
+        test = self.test_mode if test is None else test
         if test:
-            self.test = True
+            self.test_mode = True
             print(f"Translating {self.epub_path}... logging time...")
 
+        # translate the ebook (time)
         start_time = time.time()
         translated_ebook = self.translate_ebook()
         end_time = time.time()
 
+        # save translated ebook
         translated_epub_path = self.epub_path.replace(".epub", f"_translated.epub")
         epub.write_epub(translated_epub_path, translated_ebook)
 
-        if self.test:
+        # log stats (testing purposes)
+        if self.test_mode:
             # prepare the stats
             cache_stat = f"self.cached_used: {self.cached_used}"
             time_stat = f"time of translation: {end_time - start_time}"
