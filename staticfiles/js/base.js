312523574f7917569f4e8fcf21811a76bd04a077
bllen <bllendev@gmail.com>
2023-05-09 19:21:19 -0600

    static stuff

---
+++ staticfiles/js/base.js
@@ -0,0 +1,13 @@
+function getCookie(name) {
+    const value = '; ' + document.cookie;
+    const parts = value.split('; ' + name + '=');
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
