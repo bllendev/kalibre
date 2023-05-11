cb921dfff6fe293821b507ee75e9af24430a36d1
bllen <bllendev@gmail.com>
2023-05-10 20:13:46 -0600

    ai prompt improvement

--- books/constants.py
+++ books/constants.py
@@ -7,7 +7,9 @@ AI_PROMPT += "but I am not sure what I want. Can you please ask me "
 AI_PROMPT += "pertinent questions about my preferences for books, authors, "
 AI_PROMPT += "time periods, genres, and any other relevant factors "
 AI_PROMPT += "to help me discover the perfect book to read next? Ask no more "
-AI_PROMPT += "than 4 questions. "
+AI_PROMPT += "than 4 questions. Continue asking questions until we narrow "
+AI_PROMPT += "down the search to a few books, then present the best books "
+AI_PROMPT += "to me with a short description of each. "
 
 
 # UTILITY CONSTANTS
