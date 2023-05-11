a11676d566ab948dcb6be224e394f4b4460f52ea
bllen <bllendev@gmail.com>
2023-05-10 20:13:46 -0600

    ai prompt improvement

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,19 @@
 from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 from django.shortcuts import render
+from books.constants import AI_PROMPT
 
 
 @never_cache
 def home(request):
     """returns: home page view"""
-    return render(request, 'home.html')
+    return render(
+        request,
+        'home.html',
+        {
+            'AI_PROMPT': AI_PROMPT,
+        })
 
 
-class AboutPageView(TemplateView):  # new
+class AboutPageView(TemplateView):
     template_name = "about.html"
