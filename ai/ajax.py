a9bd4631a40fbb0f17b27d0172c31b45d8423693
bllen <bllendev@gmail.com>
2023-06-02 00:22:03 -0600

    create convo and messages now working as intended...

--- ai/ajax.py
+++ ai/ajax.py
@@ -1,6 +1,7 @@
 # django
 from django.http import JsonResponse
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 # tools
 import os
@@ -8,29 +9,26 @@ import json
 import openai
 
 # local
-from ai.models import TokenUsage
-from ai.constants import TOKEN_USAGE_DAILY_LIMIT
+from ai.models import TokenUsage, Conversation, Message
+from ai.constants import TOKEN_USAGE_DAILY_LIMIT, AI_PROMPT
 import tiktoken
 
 
 # update your `update_token_usage` function to use `tiktoken`
-def update_token_usage(request, messages):
+def update_token_usage(request, message):
     date_today = timezone.now().date()
     token_usage, created = TokenUsage.objects.get_or_create(user=request.user, date=date_today)
 
     # use tiktoken to count the tokens in the new messages only
     tokenizer = tiktoken.get_encoding("cl100k_base")  # moved outside of the loop
-    new_tokens = 0
-    for message in messages:
-        token_ids = list(tokenizer.encode(message["content"]))
-        new_tokens += len(token_ids)
+    token_ids = list(tokenizer.encode(message))
 
     # update the user's total tokens used
-    token_usage.tokens_used += new_tokens
+    token_usage.tokens_used += len(token_ids)
     token_usage.save()
 
 
-def query_ai(request, messages, summary=False):
+def query_ai(request, user_message, summary=False):
     """
     - sends a message to the AI Librarian (GPT-3.5 Turbo)
     - accounts for token usage (TokenUsage model)
@@ -38,21 +36,43 @@ def query_ai(request, messages, summary=False):
        the AI's response (str)
     """
     openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    # create a conversation if not exists
+    conversation_id = request.POST.get('conversation_id')
+    conversation = Conversation.objects.get(id=conversation_id, user=request.user)
+    # create a message from user
+    Message.objects.create(
+        conversation=conversation,
+        sender=Message.SENDER_USER,
+        text=user_message,
+        sent_at=timezone.now()
+    )
+    update_token_usage(request, user_message)
+
+    # get ai response using current conversation
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=messages,
+        messages=conversation.get_messages(),
         max_tokens=1000,
         temperature=0.7,
     )
-    ai_response = response['choices'][0]['message']['content'].strip()
+    ai_message = response['choices'][0]['message']['content'].strip()
+
+    # create a message from AI
+    Message.objects.create(
+        conversation=conversation,
+        sender=Message.SENDER_AI,
+        text=ai_message,
+        sent_at=timezone.now()
+    )
 
     # summarize if the user has exceeded their daily token limit
     if summary:
-        ai_response = set_latest_messages(ai_response)
+        ai_message = set_latest_messages(ai_message)
 
     # update the token usage after a successful request
-    update_token_usage(request, messages)  # updating with the correct argument
-    return ai_response
+    update_token_usage(request, ai_message)  # updating with the correct argument
+    return ai_message
 
 
 def set_latest_messages(messages):
@@ -73,36 +93,45 @@ def ai_librarian(request):
     """
     if request.method == 'POST':
         # extract messages from request
-        ai_response = None
-        messages_json = None
-        messages = None
+        ai_message = None
         try:
-            messages_json = request.POST.get('messages')
-            messages = json.loads(messages_json)
+            user_message_json = request.POST.get('user_message', "")
+            user_message = json.loads(user_message_json)
         except Exception as e:
             print(f"ERROR: {e}")
-            print(f"request.POST.items(): {request.POST.items()}")
-            print(f"messages_json: {messages_json}")
-            print(f"messages: {messages}")
 
         date_today = timezone.now().date()
         token_usage, created = TokenUsage.objects.get_or_create(user=request.user, date=date_today)
-
         try:
-
             # check if the user has exceeded their daily token limit
             if token_usage.tokens_used > TOKEN_USAGE_DAILY_LIMIT:
-                print(f"DAILY LIMIT EXCEEDED: SUMMARIZING CONVERSATION...{messages}")
-                messages.append({"role": "user", "content": "summarize the conversation succinctly thus far so that you as a generative AI model can continue the conversation meaningfully without other context."})
-                # Truncate the messages to the last two
-                messages = set_latest_messages(messages)
-                ai_response = query_ai(request, messages, summary=True)
+                return JsonResponse({'error': 'You have exceeded your daily token limit.'}, status=400)  # 400 -> bad request
             else:
-                ai_response = query_ai(request, messages)
+                ai_message = query_ai(request, user_message)
         except Exception as e:
             print(f"ai.ajax.ai_librarian ERROR: {e}")
             return JsonResponse({'error': f'An error occurred while processing your request...'}, status=500)  # 500 -> internal server error
 
-        return JsonResponse({'message': ai_response})
+        return JsonResponse({'message': ai_message})
 
     return JsonResponse({'error': 'Invalid request method'}, status=400)  # 400 -> bad request
+
+
+@csrf_exempt
+def create_conversation(request):
+    try:
+        # check if conversation_id exists in the session
+        conversation = Conversation.objects.create(user=request.user)
+        system_query = [{"role": "system", "content": AI_PROMPT}]
+        # create a message for the system prompt (ai)
+        Message.objects.create(
+            conversation=conversation,
+            sender=Message.SENDER_AI,
+            text=AI_PROMPT,
+        )
+        # return the conversation_id in the response
+        return JsonResponse({'conversation_id': conversation.id})
+
+    except Exception as e:
+        print(f"create_conversation - ERROR: {e}")
+        return JsonResponse({'error': 'Internal Server Error'}, status=500)
