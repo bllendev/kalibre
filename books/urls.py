5f44939f078d2f5d3b08b6ff2205e620dae61423
bllen <bllendev@gmail.com>
2023-06-04 15:46:26 -0600

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from books import views
 from books import ajax
 
 
+router = DefaultRouter()
+router.register('', views.BookViewSet)
+
+
 urlpatterns = [
     # core views
-    path('', views.BookListView.as_view(), name='book_list'),
+    path('', include(router.urls)),
     path('<uuid:pk>', views.BookDetailView.as_view(), name='book_detail'),
     path('search_results/', views.search_results, name='search_results'),
     path("my_emails/", views.my_emails, name="my_emails"),
