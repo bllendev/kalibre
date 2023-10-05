e356d02cf5e1f6da198bc26dc962e6de7c34bf67
bllen <bllendev@gmail.com>
2023-10-04 23:15:35 -0600

    MyProfile skeleton @method_decorator

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth import get_user_model
+from django.views.decorators.cache import never_cache
+from django.utils.decorators import method_decorator
+
 from users.forms import CustomUserCreationForm
 
 
@@ -13,6 +16,6 @@ class SignupPageView(generic.CreateView):
     template_name = "accounts/signup.html"
 
 
-
+@method_decorator(never_cache, name='dispatch')
 class MyProfile(generic.TemplateView):
     template_name = "users/my_profile.html"
