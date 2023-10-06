33957267ca144543b8509f3e043221b3097a9dec
bllen <bllendev@gmail.com>
2023-10-05 22:33:58 -0600

    my profile working !

--- users/views.py
+++ users/views.py
@@ -6,6 +6,8 @@ from django.utils.decorators import method_decorator
 
 from users.forms import CustomUserCreationForm
 
+from translate.constants import LANGUAGES
+
 
 CustomUser = get_user_model()
 
@@ -19,3 +21,23 @@ class SignupPageView(generic.CreateView):
 @method_decorator(never_cache, name='dispatch')
 class MyProfile(generic.TemplateView):
     template_name = "users/my_profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # -------------- #
+        # -- my_email -- #
+        # -------------- #
+        # build email_addresses and username_str
+        email_addresses = []
+        username_str = ''
+        if self.request.user.is_authenticated:
+            username = self.request.user.username
+            user = CustomUser.objects.get(username=username)
+            email_addresses = user.email_addresses.all()
+            username_str = f"{username}'s emails!"
+
+        context["LANGUAGES"] = LANGUAGES
+        context["email_addresses"] = email_addresses
+        context["username_str"] = username_str
+        return context
