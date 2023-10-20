491084b30393c45b2d003f837ed30b630a78edfa
bllen <bllendev@gmail.com>
2023-10-19 23:03:18 -0600

    nice inline messages

--- ai/admin.py
+++ ai/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from ai.models import TokenUsage, Conversation, Message
 
 
+# extra admin views
+class MessageInline(admin.TabularInline):  # or admin.StackedInline if you prefer a vertical layout
+    model = Message
+    extra = 1  # determines how many empty forms are displayed in the admin
+    readonly_fields = ('sender', 'text', 'sent_at')  # these fields will be displayed but not editable
+    can_delete = False  
+
+
+# admin models
+@admin.register(TokenUsage)
 class TokenUsageAdmin(admin.ModelAdmin):
     list_display = ('user', 'date', 'tokens_used')
     search_fields = ('user__username',)
@@ -9,14 +19,14 @@ class TokenUsageAdmin(admin.ModelAdmin):
     ordering = ('-date',)
 
 
-admin.site.register(TokenUsage, TokenUsageAdmin)
-
-
 @admin.register(Conversation)
 class ConversationAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'started_at', 'last_updated')
     search_fields = ('id', 'user__username',)  # assuming `username` is a field on CustomUser
     list_filter = ('started_at', 'last_updated', 'user')
+    inlines = [MessageInline]  # this adds the inline form for messages
+
+
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
