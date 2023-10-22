87798469eddec90f222ffa00f4aa31b53c5d2637
bllen <bllendev@gmail.com>
2023-10-22 00:23:16 -0600

    new working OpenLibaryAPI with get_search_results - and tests

--- books/api/_api_openlibrary.py
+++ books/api/_api_openlibrary.py
@@ -1,57 +1,33 @@
+import requests
 from bookstore_project.logging import log
 import logging
 
 logger = logging.getLogger(__name__)
 
-
 class OpenLibraryAPI:
-    """
-    {
-        "start": 0,
-        "num_found": 629,
-        "docs": [
-            {...},
-            {...},
-            ...
-            {...}]
-    }
-    .... s.t each "doc" is...
-    {
-        "cover_i": 258027,
-        "has_fulltext": true,
-        "edition_count": 120,
-        "title": "The Lord of the Rings",
-        "author_name": [
-            "J. R. R. Tolkien"
-        ],
-        "first_publish_year": 1954,
-        "key": "OL27448W",
-        "ia": [
-            "returnofking00tolk_1",
-            "lordofrings00tolk_1",
-            "lordofrings00tolk_0",
-        ],
-        "author_key": [
-            "OL26320A"
-        ],
-        "public_scan_b": true
-    }
 
-    """
-    BASE_URL = "https://openlibrary.org/search.json?"
+    BASE_URL = "https://openlibrary.org/search.json"
 
     def __init__(self):
         pass
 
-    def _get_book_search_results(self):
-        pass
+    def _get_book_search_results(self, query):
+        params = {"q": query}
+        response = requests.get(self.BASE_URL, params=params)
+        
+        # It's better to check the response status code
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
 
     @log
-    def get_book_search_results(self):
+    def get_book_search_results(self, query):
+        book_search_results = None
         try:
-            book_list = self._book_search_results()
+            _book_search_results = self._get_book_search_results(query)
+            book_search_results = _book_search_results["docs"]
         except Exception as e:
             logger.error(f"ERROR: books.api._api_openlibrary.get_book_search_results: {e}")
             raise e
-
-    
+        return book_search_results
--- books/tests/test_api_openlibrary.py
+++ books/tests/test_api_openlibrary.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from books.api._api_openlibrary import OpenLibraryAPI
 
 
@@ -9,27 +9,71 @@ docker compose exec web python manage.py test books.tests.test_api_openlibrary -
 
 
 class TestOpenLibraryAPI(unittest.TestCase):
+    """
+    """
 
     def setUp(self):
         self.api = OpenLibraryAPI()
+    
+    # def test_get_book_search_results(self):
+    #     """tests the calling of the api, leave this commented as it tests the api through a real query"""
+    #     book_list = self.api._get_book_search_results(query="my sweet orange tree")
+        
+    #     self.assertTrue(book_list)
+    #     self.assertTrue(f"{book_list['docs']}")
+
+    @patch('books.api._api_openlibary.requests.get')
+    def test_get_book_search_results_success(self, mock_get):
+        """"""
+        mock_response = Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            "start": 0,
+            "num_found": 1,
+            "docs": [
+                {
+                    "title": "Test Book",
+                    "author_name": ["Test Author"],
+                }
+            ]
+        }
+        mock_get.return_value = mock_response
+
+        api = OpenLibraryAPI()
+        results = api.get_book_search_results("Test Book")
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0]["title"], "Test Book")
+
+    @patch('books.api._api_openlibary.requests.get')
+    def test_get_book_search_results_failure(self, mock_get):
+        # mocking a failed response from the requests.get method
+        mock_response = Mock()
+        mock_response.status_code = 404
+        mock_response.raise_for_status.side_effect = Exception("Not Found")
+        mock_get.return_value = mock_response
+
+        api = OpenLibraryAPI()
+        with self.assertRaises(Exception) as context:
+            api.get_book_search_results("Non-existent Book")
+        self.assertTrue("Not Found" in str(context.exception))
 
     # @patch.object(OpenLibraryAPI, '_get_book_search_results')
     # def test_get_book_search_results_success(self, mock_get_book_search_results):
-    #     # Mock the return value of the private method to simulate successful response
+    #     # mock the return value of the private method to simulate successful response
     #     mock_get_book_search_results.return_value = ["book1", "book2", "book3"]
 
-    #     # Test
+    #     # test
     #     result = self.api.get_book_search_results()
 
-    #     # Assert
+    #     # assert
     #     self.assertEqual(result, ["book1", "book2", "book3"])
 
     # @patch.object(OpenLibraryAPI, '_get_book_search_results')
     # def test_get_book_search_results_failure(self, mock_get_book_search_results):
-    #     # Mock the method to raise an exception to simulate failure scenario
+    #     # mock the method to raise an exception to simulate failure scenario
     #     mock_get_book_search_results.side_effect = Exception("An error occurred!")
 
-    #     # Test and Assert
+    #     # aest and assert
     #     with self.assertRaises(Exception) as context:
     #         self.api.get_book_search_results()
     #     self.assertEqual(str(context.exception), "An error occurred!")
