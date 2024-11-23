cc140524d26791900e08221db867996b03a5168b
Allen Garza <bllendev@gmail.com>
2024-11-23 12:07:41 -0600

    Changelog: added migrate for author model update

--- authors/factory/author.py
+++ authors/factory/author.py
@@ -1,13 +1,11 @@
-
 import factory
+
 
 class AuthorFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "authors.Author"
 
-    id = factory.Faker('uuid4')
-    name = factory.Faker('name')
-    alias = factory.Faker('word')
-    birth_date = factory.Faker('date_of_birth')
-    death_date = factory.Faker('date_this_century')
-    webpage = factory.Faker('url')
+    id = factory.Faker("uuid4")
+    key = factory.Faker("bothify", text="/authors/OL#####A")
+    name = factory.Faker("name")
+    birth_date = factory.Faker("date")
